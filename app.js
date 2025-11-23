de729eb87a8a27555dc8d262d8ae65f428b7f497
swachand <swachandj@gmail.com>
2025-11-23 19:30:25 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from Kubernetes CI/CD demo!");
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
